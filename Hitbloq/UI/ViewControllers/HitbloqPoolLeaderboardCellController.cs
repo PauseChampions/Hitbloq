abfc3bcb7156e28114a7a586e0ac1428693c973f
PixelBoom <rithikbhatia12@gmail.com>
2022-04-24 05:14:15 -0400

    Added leaderboard cell background

--- Hitbloq/UI/ViewControllers/HitbloqPoolLeaderboardCellController.cs
+++ Hitbloq/UI/ViewControllers/HitbloqPoolLeaderboardCellController.cs
@@ -3,8 +3,10 @@ using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components;
 using Hitbloq.Entries;
 using Hitbloq.Other;
+using Hitbloq.Utilities;
 using HMUI;
 using Tweening;
 using UnityEngine;
@@ -18,14 +20,18 @@ namespace Hitbloq.UI.ViewControllers
         private SpriteLoader spriteLoader = null!;
         private MaterialGrabber materialGrabber = null!;
         private TweeningManager uwuTweenyManager = null!;
-        
+
         [UIComponent("profile-picture")]
         private readonly ImageView profilePicture = null!;
-        
+
         [UIAction("#post-parse")]
         private void PostParse()
         {
             profilePicture.material = materialGrabber.NoGlowRoundEdge;
+            
+            fogBG = background.background.material;
+            roundRectSmall = background.background.sprite;
+            originalBackgroundColour = background.background.color;
         }
 
         public void SetRequiredUtils(SpriteLoader spriteLoader, MaterialGrabber materialGrabber, TweeningManager uwuTweenyManager)
@@ -44,6 +50,7 @@ namespace Hitbloq.UI.ViewControllers
             
             this.poolLeaderboardEntry = poolLeaderboardEntry;
             _ = FetchProfilePicture();
+            _ = FetchBackground();
 
             NotifyPropertyChanged(nameof(Rank));
             NotifyPropertyChanged(nameof(Username));
@@ -55,7 +62,6 @@ namespace Hitbloq.UI.ViewControllers
         
         private async Task FetchProfilePicture()
         {
-            uwuTweenyManager.KillAllTweens(profilePicture);
             var currentEntry = poolLeaderboardEntry;
             profilePicture.sprite = BeatSaberMarkupLanguage.Utilities.ImageResources.BlankSprite;
 
@@ -79,6 +85,54 @@ namespace Hitbloq.UI.ViewControllers
                 }
             });
         }
+
+        #region Background
+
+        private Sprite? roundRectSmall;
+        private Material? fogBG;
+        
+        private Color? originalBackgroundColour;
+        private readonly Color customBackgroundColour = new(0.5f, 0.5f, 0.5f, 1f);
+        
+        [UIComponent("background")]
+        private readonly Backgroundable background = null!;
+        
+        private async Task FetchBackground()
+        {
+            if (background.background is not ImageView bgImageView)
+            {
+                return;
+            }
+            
+            var currentEntry = poolLeaderboardEntry;
+            
+            bgImageView.sprite = roundRectSmall;
+            bgImageView.overrideSprite = roundRectSmall;
+            bgImageView.color = originalBackgroundColour!.Value;
+            bgImageView.material = fogBG;
+            Accessors.GradientAccessor(ref bgImageView) = false;
+
+            if (poolLeaderboardEntry is not {BannerImageURL:{}})
+            {
+                return;
+            }
+            
+            await spriteLoader.DownloadSpriteAsync(poolLeaderboardEntry.BannerImageURL, sprite =>
+            {
+                if (poolLeaderboardEntry == currentEntry)
+                {
+                    bgImageView.sprite = sprite;
+                    bgImageView.overrideSprite = sprite;
+                    bgImageView.color = customBackgroundColour;
+                    bgImageView.color0 = new Color(0.5f, 0.5f, 0.5f, 1f);
+                    bgImageView.color1 = Color.white;
+                    Accessors.GradientAccessor(ref bgImageView) = true;
+                    bgImageView.material = materialGrabber.NoGlowRoundEdge;
+                }
+            });
+        }
+
+        #endregion
 
         [UIValue("rank")] 
         private string Rank => poolLeaderboardEntry?.Rank.ToString() ?? "";
